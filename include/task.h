fb0003b78385d628e952062a9512a9cd2b3c1629
Pg59907 <pg59907@uminho.pt>
2026-01-02 03:07:27 +0000

    Optimizações 1 e 2

---
+++ include/task.h
@@ -0,0 +1,12 @@
+#ifndef TASK_H
+#define TASK_H
+
+
+// DEFINIÇÃO DA ESTRUTURA TASK
+typedef struct task {
+    int id; // identificador da tarefa
+    int duration; // duração em segundos
+} Task;
+
+
+#endif
